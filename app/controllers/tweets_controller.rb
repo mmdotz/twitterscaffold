edae97b2dc1fd8e36f1852cc1e8ffbf5676681f2
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-01 19:05:33 -0500

    create new tweet working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,10 +31,8 @@ class TweetsController < ApplicationController
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
